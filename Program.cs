2acc8b3db5e939b08555875f991f0663580f8601
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-19 15:52:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FileStreamStreamReader2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
